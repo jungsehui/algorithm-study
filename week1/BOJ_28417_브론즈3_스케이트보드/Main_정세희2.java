4a16e4941030f53302eae067f2198641459a33ae
jungsehui <jungsehui@naver.com>
2024-12-13 19:27:48 +0900

    feat: 두 번째 풀이

--- week1/BOJ_28417_브론즈3_스케이트보드/Main_정세희2.java
+++ week1/BOJ_28417_브론즈3_스케이트보드/Main_정세희2.java
@@ -1,9 +1,14 @@
-package week1;
+package week1.BOJ_28417_브론즈3_스케이트보드;
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
 
-public class Main {
+public class Main_정세희2 {
 
     public static void main(String[] args) throws IOException {
 
