ceea99b9fecbecce2e37ee78ac74e71384fa8fe8
jungsehui <jungsehui@naver.com>
2024-12-14 00:33:26 +0900

    feat: 첫 번째 구현 그러나 만약에 entrySet() 사용법이 잘 기억나지 않는다면 ? 연습 중에는 구글링이 가능하지만 실제 코테라면 구글링 없이 .. 해 봤자 공식 문서만 준다면 거기서 찾기 어려울 거기 때문에 두 번째 그냥 원시적인 풀이를 하려고 한다

--- week1/BOJ_1159_브론즈2_농구_경기/Main_정세희.java
+++ week1/BOJ_1159_브론즈2_농구_경기/Main_정세희.java
@@ -1,10 +1,15 @@
-package week1;
+package week1.BOJ_1159_브론즈2_농구_경기;
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
 import java.util.stream.Collectors;
 
-public class Main {
+public class Main_정세희 {
 
     public static void main(String[] args) throws IOException {
 
