4413e96998c60e1ee13348198982f807b639829b
jungsehui <jungsehui@naver.com>
2024-12-16 22:15:37 +0900

    feat: 영식이와 친구들 구현

--- week1/BOJ_1592_브론즈2_영식이와친구들/Main_정세희.java
+++ week1/BOJ_1592_브론즈2_영식이와친구들/Main_정세희.java
@@ -1,9 +1,11 @@
-package week1;
+package week1.BOJ_1592_브론즈2_영식이와친구들;
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public class Main {
+public class Main_정세희 {
 
     public static void main(String[] args) throws IOException {
 
