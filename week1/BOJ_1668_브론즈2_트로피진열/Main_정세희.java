5567a20047ce6258afd844d841a6d1dc211a94dd
jungsehui <jungsehui@naver.com>
2024-12-18 00:51:10 +0900

    feat: 트로피 진열 풀이

--- week1/BOJ_1668_브론즈2_트로피진열/Main_정세희.java
+++ week1/BOJ_1668_브론즈2_트로피진열/Main_정세희.java
@@ -1,9 +1,14 @@
-package week1;
+package week1.BOJ_1668_브론즈2_트로피진열;
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
 
-public class Main {
+public class Main_정세희 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
