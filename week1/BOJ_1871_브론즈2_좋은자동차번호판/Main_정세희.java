d9d45901d307bda21cc76f3ab390505c30407c98
jungsehui <jungsehui@naver.com>
2024-12-18 20:27:49 +0900

    feat: 좋은 자동차 번호판

--- week1/BOJ_1871_브론즈2_좋은자동차번호판/Main_정세희.java
+++ week1/BOJ_1871_브론즈2_좋은자동차번호판/Main_정세희.java
@@ -1,9 +1,13 @@
-package week1;
+package week1.BOJ_1871_브론즈2_좋은자동차번호판;
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
-public class Main {
+public class Main_정세희 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
